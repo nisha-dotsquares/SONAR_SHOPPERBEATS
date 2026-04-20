9bd5858b96fefbed9851d2d77ee5ee396c4bf872
nisha-dotsquares <nisha.lalwani@dotsquares.com>
2026-04-20 23:27:59 +0530

    Delete components directory

--- components/homepage/RecentlyViewedSection.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import RecentlyViewed from "./RecentlyViewed";
-import { Product } from "@/types/product";
-
-export default function RecentlyViewedSection({ recentlyViewed }: { recentlyViewed: Product[] }) {
-  if (!recentlyViewed || recentlyViewed.length === 0) return null;
-  return <RecentlyViewed recentlyViewed={recentlyViewed} />;
-}
