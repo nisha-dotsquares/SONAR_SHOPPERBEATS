ddfe5575ba354cfb044cbcd0de97e6ddd01cfd6e
nisha-dotsquares <nisha.lalwani@dotsquares.com>
2026-04-21 00:21:43 +0530

    Delete types directory

--- types/cms.ts
+++
@@ -1,4 +0,0 @@
-export interface LegalSection {
-  title: string;
-  content: string[];
-}
--- types/zendesk.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    zE?: (service: string, event: string, ...args: (string | Record<string, unknown>)[]) => void;
-  }
-}
